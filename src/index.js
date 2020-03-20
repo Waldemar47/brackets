e903988e1ad0bf9b2bfd10a9d77dbdb28f74028c
Waldemar47 <w.karsten@tut.by>
2020-03-20 17:23:52 +0300

    init startund create index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let roll = []; 
+    let marker = false; 
+    for(let a = 0; a < str.length; a++) {
+      marker = false; 
+      for(let b = 0; b < bracketsConfig.length; b++) {
+        if (str[a] == bracketsConfig[b][1]) {
+          if (roll[roll.length - 1] == bracketsConfig[b][0]) {
+            marker = true;
+            roll.pop();   
+          }
+          break;
+        }
+      } 
+      if (!marker) {
+        for(let b = 0; b < bracketsConfig.length; b++) {
+          if (str[a] == bracketsConfig[b][0]) {
+            roll.push(str[a]); 
+            marker = true; 
+            break; 
+          }
+        } 
+      }
+      if (!marker) return false; 
+    }
+    if (roll.length == 0) return true;
+    else return false; 
+  }
